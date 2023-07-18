5ecede641508e3fe2e3f92842808a4c7c65e6396
morgane-marechal <morgane.marechal@laplateforme.io>
2023-07-18 16:40:59 +0200

    improve css

--- src/View/header.php
+++ src/View/header.php
@@ -5,17 +5,17 @@
 
             <nav>
             <button role="icon-button">X</button>
-            <li><a href="home">Home sweet home</a></li>
-                <li><a href="movies">Les films</a></li>
-                <li><a href="tvshow">Les séries</a></li>
+                <li ><a href="home" class="menuLink">Home sweet home</a></li>
+                <li><a href="movies" class="menuLink">Les films</a></li>
+                <li><a href="tvshow" class="menuLink">Les séries</a></li>
             <?php if (empty($_SESSION['id'])){ ?>
-                <li><a href="register">S'enregistrer</a></li>
-                <li><a href="login">Se connecter</a></li>
+                <li><a href="register" class="menuLink">S'enregistrer</a></li>
+                <li><a href="login" class="menuLink">Se connecter</a></li>
             <?php } ?>
 
             <?php if (isset($_SESSION['id'])){ ?>
-                <li><a href="account">Votre compte</a></li>
-                <li><a href="logout">Déconnexion</a></li>
+                <li><a href="account" class="menuLink">Votre compte</a></li>
+                <li><a href="logout" class="menuLink">Déconnexion</a></li>
             <?php } ?>
 
             </nav>
